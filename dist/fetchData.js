4cf0ba809be9ea8b894120e9f0a7193f085a30a0
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2023-09-06 11:28:56 -0300

    Tabela, Total,CountBy & MelhorDia

--- dist/fetchData.js
+++ dist/fetchData.js
@@ -2,7 +2,7 @@ export default async function fetchData(url) {
     try {
         const response = await fetch(url);
         if (!response.ok)
-            throw new Error("Erro:" + response.status);
+            throw new Error("Erro: " + response.status);
         const json = await response.json();
         return json;
     }
--- dist/normalizarTransacao.js
+++ dist/normalizarTransacao.js
@@ -1,12 +1,14 @@
+import moedaParaNumero from "./moedaParaNumero.js";
+import stringToDate from "./stringToDate.js";
 export default function normalizarTransacao(transacao) {
     return {
         nome: transacao.Nome,
-        id: transacao.Id,
-        data: transacao.Data,
+        id: transacao.ID,
+        data: stringToDate(transacao.Data),
         status: transacao.Status,
         email: transacao.Email,
         moeda: transacao["Valor (R$)"],
-        valor: 0,
+        valor: moedaParaNumero(transacao["Valor (R$)"]),
         pagamento: transacao["Forma de Pagamento"],
         novo: Boolean(transacao["Cliente Novo"]),
     };
